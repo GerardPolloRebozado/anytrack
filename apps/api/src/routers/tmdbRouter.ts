46b99bcd9694d853859c56c5def2af70b6576d1b
gerardPolloRebozado <gerardit4@gmail.com>
2024-06-11 15:49:00 +0200

    feat: :sparkles: Added cast overview to the show page

--- apps/api/src/routers/tmdbRouter.ts
+++ apps/api/src/routers/tmdbRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getShowSeasons } from "../controllers/tmdbController";
+import { getCredits, getShowSeasons } from "../controllers/tmdbController";
 import joiMiddleware from "../middlewares/joiMiddleware";
 import { userMiddleware } from "../middlewares/userMiddleware";
-import { getShowSeasonsSchema } from "@anytrack/joi";
+import { getCreditsSchema, getShowSeasonsSchema } from "@anytrack/joi";
 
 const tmdbRouter = Router();
 
 tmdbRouter.get("/show/seasons", userMiddleware, joiMiddleware(getShowSeasonsSchema, 'query'), getShowSeasons);
+tmdbRouter.get('/credits', userMiddleware, joiMiddleware(getCreditsSchema, 'query'), getCredits)
 
 export default tmdbRouter;
--- libs/joi/src/index.ts
+++ libs/joi/src/index.ts
@@ -1,76 +1,87 @@
 import Joi from "joi";
 import { tlds } from "@hapi/tlds";
 export { Joi };
+import { MediaType } from '@prisma/client';
 
 export const createUserSchema = Joi.object({
-    confirmPassword: Joi.ref("password"),
-    email: Joi.string().email({ tlds: { allow: tlds } }).required(),
-    name: Joi.string().required(),
-    password: Joi.string().min(6).required(),
+  confirmPassword: Joi.ref("password"),
+  email: Joi.string().email({ tlds: { allow: tlds } }).required(),
+  name: Joi.string().required(),
+  password: Joi.string().min(6).required(),
 });
 
 export const loginUserSchema = Joi.object({
-    email: Joi.string().email({ tlds: { allow: tlds } }).required(),
-    password: Joi.string().min(6).required(),
+  email: Joi.string().email({ tlds: { allow: tlds } }).required(),
+  password: Joi.string().min(6).required(),
 });
 
 export const searchMoviesSchema = Joi.object({
-    query: Joi.string().required(),
+  query: Joi.string().required(),
 });
 
 export const searchMovieByIdSchema = Joi.object({
-    id: Joi.string().required(),
+  id: Joi.string().required(),
 });
 
 export const deleteMediaSchema = Joi.object({
-    id: Joi.number().required(),
+  id: Joi.number().required(),
 })
 
 export const markMediaSchema = Joi.object({
-    tmdbId: Joi.alternatives(Joi.string(), Joi.number()).required(),
-    watched: Joi.boolean().required(),
-    watchedDate: Joi.date().optional(),
-    mediaType: Joi.string().valid("movie", "show").required
-        (),
+  tmdbId: Joi.alternatives(Joi.string(), Joi.number()).required(),
+  watched: Joi.boolean().required(),
+  watchedDate: Joi.date().optional(),
+  mediaType: Joi.string().valid("movie", "show").required
+    (),
 })
 
 export const getMarkedMediaSchema = Joi.object({
-    mediaType: Joi.string().valid("movie", "show").optional(),
-    watched: Joi.boolean().optional(),
-    groupBy: Joi.string().valid('month').optional(),
+  mediaType: Joi.string().valid("movie", "show").optional(),
+  watched: Joi.boolean().optional(),
+  groupBy: Joi.string().valid('month').optional(),
 })
 
 export const getShowSchema = Joi.object({
-    term: Joi.string().required(),
+  term: Joi.string().required(),
 })
 
 export const markShowSchema = Joi.object({
-    watchedDate: Joi.date().optional().max("now"),
-    watched: Joi.boolean().required(),
-    season: Joi.number().required(),
-    episode: Joi.number().optional()
+  watchedDate: Joi.date().optional().max("now"),
+  watched: Joi.boolean().required(),
+  season: Joi.number().required(),
+  episode: Joi.number().optional()
 })
 
 export const markMovieSchema = Joi.object({
-    tmdbId: Joi.string().required(),
-    watchedDate: Joi.date().max("now").optional(),
-    watched: Joi.boolean().required()
+  tmdbId: Joi.string().required(),
+  watchedDate: Joi.date().max("now").optional(),
+  watched: Joi.boolean().required()
 })
 
 export const markMovieSchemaForm = Joi.object({
-    watchedDate: Joi.date().max("now").required(),
-    watched: Joi.boolean().required()
+  watchedDate: Joi.date().max("now").required(),
+  watched: Joi.boolean().required()
 })
 
 export const getShowSeasonsSchema = Joi.object({
-    tmdbId: Joi.string().required(),
-    season: Joi.number().optional()
+  tmdbId: Joi.string().required(),
+  season: Joi.number().optional()
 })
 
 export const getMarkedMoviesSchema = Joi.object({
-    groupBy: Joi.string().valid('month').optional()
+  groupBy: Joi.string().valid('month').optional()
 })
 
 export const removeMarkedMovieSchema = Joi.object({
-    id: Joi.number().required()
+  id: Joi.number().required()
+})
+
+export const getCreditsSchema = Joi.object({
+  tmdbId: Joi.string().required(),
+  mediaType: Joi.string().valid(...Object.values(MediaType)).required(),
+  season: Joi.number().when('mediaType', {
+    is: MediaType.show,
+    then: Joi.optional(),
+    otherwise: Joi.forbidden()
+  })
 })
