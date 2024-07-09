a6a2e6231f0d9b8e710e2a630f9be0456d1c20cb
gerardPolloRebozado <gerardit4@gmail.com>
2024-07-09 17:57:40 +0200

    feat: :sparkles: Now the input component will show an eye on password input to see the password

--- apps/web/src/components/Input/Input.tsx
+++ apps/web/src/components/Input/Input.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import Callout from '../Callout/Callout';
 import styles from './Input.module.css';
+import { Eye, EyeOff } from 'lucide-react';
+import PrimaryButton from '../PrimaryButton/PrimaryButton';
 
 export default function Input({ label, register, name, type, placeholder = "", error, className }: {
   label: string;
@@ -10,13 +13,17 @@ export default function Input({ label, register, name, type, placeholder = "", e
   error: any;
   className?: string;
 }) {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className={className}>
       <label htmlFor={name} className={styles.label}>
         {label + ":"}
       </label>
-      <input id={name} {...register(name)} type={type} placeholder={placeholder} className={styles.input}
-      />
+      <div className={styles.inputEyeContainer}>
+        <input id={name} {...register(name)} type={type === "password" && showPassword ? "text" : type} placeholder={placeholder} className={styles.input}
+        /> {type === "password" && showPassword && <PrimaryButton onClick={() => setShowPassword(false)}><Eye className={styles.eye} color='black' /></PrimaryButton>}
+        {type === "password" && !showPassword && <PrimaryButton onClick={() => setShowPassword(true)}><EyeOff className={styles.eye} color='black' /></PrimaryButton>}
+      </div>
       {error && <Callout type="error">{error.message}</Callout>}
     </div>
   );
