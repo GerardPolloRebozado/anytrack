20f464edeed0977839c0eaa98a9809a73d2d6c5e
gerardPolloRebozado <gerardit4@gmail.com>
2024-07-10 16:10:10 +0200

    feat: ✨ Started migration to shadcn/ui. signin and signup

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "@/components/Providers/themeProvider";
+import { Montserrat as FontSans } from "next/font/google";
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/utils/utils";
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "AnyTrack",
@@ -17,8 +22,16 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={montserrat.className}>
-        {children}</body>
-    </html>
+      <body className={cn("bg-background font-sans antialiased", fontSans.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html >
   );
 }
