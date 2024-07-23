96f295d3ee3ed6bd162d6b94fe5524d2350f400f
gerardPolloRebozado <gerardit4@gmail.com>
2024-07-23 10:48:18 +0200

    feat: :sparkles: added watchProviders api endpoint

--- apps/api/src/controllers/tmdbController.ts
+++ apps/api/src/controllers/tmdbController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { getAgregatedShowCreditsService, getMovieCredits, getOnePeopleService, searchShowSeasonsService, searchShowTmdbIdService } from '../services/tmdbService';
+import { getAgregatedShowCreditsService, getMovieCredits, getMovieProviders, getOnePeopleService, getShowProviders, searchShowSeasonsService, searchShowTmdbIdService } from '../services/tmdbService';
 import { MediaType } from '@anytrack/type';
 
 export const getShowSeasons = async (req: Request, res: Response) => {
@@ -69,6 +69,51 @@ export const getOnePeople = async (req: Request, res: Response) => {
       media.poster_path = `https://image.tmdb.org/t/p/original${media.poster_path}`;
     });
     res.status(200).json(people);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export const getWatchProviders = async (req: Request, res: Response) => {
+  try {
+    const tmdbId = Number(req.params.tmdbId)
+    const mediaType = req.query.mediaType
+    let providers
+    switch (mediaType) {
+      case MediaType.movie: {
+        const responseMovie = await getMovieProviders(tmdbId)
+        providers = await responseMovie.json()
+        break;
+      }
+      case MediaType.show: {
+        const responseShow = await getShowProviders(tmdbId)
+        providers = await responseShow.json()
+        break;
+      }
+      default:
+        res.status(500).json('Only shows and movies can be used to check watch providers')
+        return
+    }
+    Object.values(await providers.results).map((provider: any) => {
+      console.log(provider);
+      if (provider.flatrate) (
+        provider.flatrate.map((flatrate: any) => {
+          flatrate.logo_path = `https://image.tmdb.org/t/p/original${flatrate.logo_path}`
+        })
+      )
+      if (provider.rent) (
+        provider.rent.map((rent: any) => {
+          rent.logo_path = `https://image.tmdb.org/t/p/original${rent.logo_path}`
+        })
+      )
+      if (provider.buy) (
+        provider.buy.map((buy: any) => {
+          buy.logo_path = `https://image.tmdb.org/t/p/original${buy.logo_path}`
+        })
+      )
+    })
+    res.status(200).json(providers)
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
--- apps/api/src/routers/tmdbRouter.ts
+++ apps/api/src/routers/tmdbRouter.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { getCredits, getOnePeople, getShowSeasons } from "../controllers/tmdbController";
+import { getCredits, getOnePeople, getShowSeasons, getWatchProviders } from "../controllers/tmdbController";
 import joiMiddleware from "../middlewares/joiMiddleware";
 import { userMiddleware } from "../middlewares/userMiddleware";
-import { getCreditsSchema, getShowSeasonsSchema } from "@anytrack/joi";
+import { getCreditsSchema, getShowSeasonsSchema, getWatchProvidersParamsSchema, getWatchProvidersQuerySchema } from "@anytrack/joi";
 
 const tmdbRouter = Router();
 
 tmdbRouter.get("/show/seasons", userMiddleware, joiMiddleware(getShowSeasonsSchema, 'query'), getShowSeasons);
 tmdbRouter.get('/credits', userMiddleware, joiMiddleware(getCreditsSchema, 'query'), getCredits)
 tmdbRouter.get('/people/:peopleId', userMiddleware, getOnePeople)
+tmdbRouter.get("/providers/:tmdbId", userMiddleware, joiMiddleware(getWatchProvidersParamsSchema, 'params'), joiMiddleware(getWatchProvidersQuerySchema, 'query'), getWatchProviders)
 
 export default tmdbRouter;
