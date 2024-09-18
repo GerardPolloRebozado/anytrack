d5bb608fe67042b90d0b6a034924d840069d70a1
gerardPolloRebozado <gerardit4@gmail.com>
2024-09-18 13:41:41 +0200

    fix: :ambulance: fix schema location

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,5 @@
 echo "Runing database migrations"
-npx prisma migrate deploy --schema
+npx prisma migrate deploy
 npx nx reset
 echo "Starting the app"
 npx npm-run-all --parallel start:web start:api
