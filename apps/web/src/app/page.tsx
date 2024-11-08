14f40966860097952177b5282e322a9e95c5def0
gerardPolloRebozado <gerardit4@gmail.com>
2024-11-08 12:03:09 +0100

    ci: :rotating_light: fix compiler warning in nextjs

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter();
-  router.replace('/dashboard')
+  const router = useRouter()
+  useEffect(() => {
+    router.replace('/dashboard');
+  }, [router]);
   return (
     <main>
     </main>
